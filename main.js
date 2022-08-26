12209b17c40b942fe0b8f9bea0919552c24f896c
Son Do <dominhson580@gmail.com>
2022-08-26 14:38:30 +0700

    Show image when clicj corresponding dot

--- main.js
+++ main.js
@@ -12,3 +12,28 @@ function showItems(event) {
   const icon = dropdown.querySelector("i");
   icon.classList.toggle("show");
 }
+
+// IMAGE SLIDER
+const images = document.querySelectorAll(".images img");
+
+const dots = document.querySelectorAll(".dots .dot");
+dots.forEach((dot) => dot.addEventListener("click", showImage));
+
+function showImage(event) {
+  const activeDot = event.target;
+  if (activeDot.classList.contains("active")) {
+    return;
+  }
+  const currentDot = document.querySelector(".dot.active");
+  currentDot.classList.remove("active");
+  activeDot.classList.add("active");
+
+  const index = activeDot.getAttribute("data-index");
+  const activeImage = document.querySelector(`img[data-index="${index}"]`);
+  const currentImage = document.querySelector("img.active");
+  currentImage.classList.remove("active");
+  activeImage.classList.add("active");
+}
+
+const leftArrow = document.querySelector("arrows .left");
+const rightArrow = document.querySelector("arrows .right");
